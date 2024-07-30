d6b53bcf8ecd235b300ec09c16fc92c455c6188b
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-07-30 20:53:01 +0300

    stock models added

--- main/level_up/models.py
+++ main/level_up/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+
+from django.db import models
+
+
+class Boost(models.Model):
+    pass
+
+
+from django.db import models
+
+
+class Player(models.Model):
+    player_id = models.CharField(max_length=100)
+
+
+class Level(models.Model):
+    title = models.CharField(max_length=100)
+    order = models.IntegerField(default=0)
+
+
+class Prize(models.Model):
+    title = models.CharField()
+    # id
+
+
+class PlayerLevel(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    completed = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    score = models.PositiveIntegerField(default=0)
+
+
+class LevelPrize(models.Model):
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    prize = models.ForeignKey(Prize, on_delete=models.CASCADE)
+    received = models.DateField()
+
